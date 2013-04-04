f21838cd3e909e485b9510af53e4eebb40ccdc9a
Toby Wilson <toby@livingskincare.com.au>
2013-04-04 22:12:41 +1100

    Here's the first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadApp::Application.config.secret_token = 'f5099ad500c3505a6e0aba5cddfb945e4b93f5e1ef24be9c731134d8a5abe119aa95e7f9625d4a0aa05b8fdf7a5c2662e633e39abe5e36ed2aee70b7395b2f21'
